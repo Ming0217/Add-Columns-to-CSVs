e817af3ac792e435db6537e8cf5995946570963b
Ming Huang <mings430@gmail.com>
2020-05-13 20:37:45 -0400

    refactor the code

--- add_new_columns.py
+++ add_new_columns.py
@@ -5,15 +5,17 @@ import click
 @click.command()
 @click.argument('files', nargs=-1)
 
+
 def add_new_columns(files):
+    
+    new_columns = ["column1", "column2", "column3", "column4", "column5", "column6"]
+    
     for file in files:
         df = pd.read_csv(file)
-        df["column1"] = ""
-        df["column2"] = ""
-        df["column3"] = ""
-        df["column4"] = ""
-        df["column5"] = ""
-        df["column6"] = ""
+        for column in new_columns:
+            df[column] = ""
+
+       #df.rename({'old_column1':'new_column1','old_column2':'new_column2'},axis=1) #this is used to rename columns
         df.to_csv(file, index=False)
 
 if __name__=="__main__":
