6e3be36885ac84e485033dab87c400d06d33a19e
Ming Huang <mings430@gmail.com>
2021-01-14 20:54:25 -0500

    Update add_new_columns.py

--- add_new_columns.py
+++ add_new_columns.py
@@ -11,11 +11,14 @@ def add_new_columns(files):
     new_columns = ["column1", "column2", "column3", "column4", "column5", "column6"]
     
     for file in files:
-        df = pd.read_csv(file)
+        df = pd.read_csv(file, dtype="object", keep_default_na=False, encoding="windows-1252")
         for column in new_columns:
             df[column] = ""
-
+            
+       #df.drop(columns=['column1'], inplace=True) #this is used to remove columns
+    
        #df.rename({'old_column1':'new_column1','old_column2':'new_column2'},axis=1) #this is used to rename columns
+        
         df.to_csv(file, index=False)
 
 if __name__=="__main__":
